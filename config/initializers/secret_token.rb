080d14eb46679e28287203b01cf54d15f211cf3b
Phill Baker <phillbaker@retrodict.com>
2012-09-16 11:13:19 -0400

    Semi-funky bootstrap into subdirectory and the mv into current. Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ihub::Application.config.secret_token = '2c7dfa6f24aee95675f2dffe0e17817cfe3303ef95374875fa616c7e56480153a12141a72f24e6c416a39fac085df3daea169e8106377ed42f063cabcfc8fcfc'
