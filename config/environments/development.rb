a9aed3df7f861d347bb8620a8949a745a578ba90
Phill Baker <phillbaker@retrodict.com>
2012-09-16 11:57:42 -0400

    Add manual setup for devise.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ Ihub::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+  
+  # For Devise: default url options
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
