15f352f5475eda8f29f266545dc13613e9e520b9
Phill Baker <phillbaker@retrodict.com>
2012-09-16 16:30:53 -0400

    Add relationships.

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,5 +2,8 @@ class Contact
   include DataMapper::Resource
 
   property :id, Serial
-
+  property :name, String
+  property :telephone, Integer
+  
+  belongs_to :hub
 end
--- app/models/hub.rb
+++ app/models/hub.rb
@@ -2,6 +2,8 @@ class Hub
   include DataMapper::Resource
 
   property :id, Serial
+  property :name, String
   
   belongs_to :user
+  has n, :contacts
 end
