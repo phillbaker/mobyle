798c3e5b3e7bf70f2290c1f63945a87808c01f3e
Phill Baker <phillbaker@retrodict.com>
2012-11-20 08:16:14 -0500

    Add front images.

--- app/assets/javascripts/mobile/mobile.js
+++ app/assets/javascripts/mobile/mobile.js
@@ -7,4 +7,24 @@
 
 $(document).ready(function() {
   //For manually testing/forcing add2home: addToHome.show(true);
+  
+  // http://stackoverflow.com/a/8000626
+  var stickFooter = function() {
+    $('[data-role=content]').each(function() {
+      var containerHeight = parseInt($(this).css('height'));
+      var windowHeight = parseInt(window.innerHeight);
+      var footerAndHeaderHeight = 115;//this assumes both header and footer are turned on
+      if(containerHeight+footerAndHeaderHeight < windowHeight) {
+          var newHeight = windowHeight-footerAndHeaderHeight;
+          $(this).css('min-height', newHeight+'px');
+      }
+    });
+  };
+  
+  // Run the stick footer once on page load and once on orientation change
+  stickFooter();
+  $(document).bind('orientationchange', function(event) {
+      stickFooter();
+  });
+  
 });
